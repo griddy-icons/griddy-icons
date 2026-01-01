71e9a1d43d727380a6c77c733d8841542317380e
Zuzana <zuzikbenova@gmail.com>
2026-01-01 15:27:08 +0100

    improve icons library build

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: ["src/index.ts"],
-  dts: true,
+  entry: ["src/**/*.{ts,tsx}", "!src/**/*.d.ts", "!src/scripts/**/*"],
+  dts: false,
   format: ["esm", "cjs"],
   clean: false, // Clean is handled by the build script sequence now
   target: "es2019",
