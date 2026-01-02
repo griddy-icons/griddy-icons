4ee4ac047dc90dc8c838619d173eb3299b0ea0c3
Zuzana <zuzikbenova@gmail.com>
2026-01-02 19:19:07 +0100

    fix: remove or replace broken icons

--- src/icons/CursorText/regular.tsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-export default (
-  <>
-    <path fillRule="evenodd" clipRule="evenodd" d="M9 2H8v1.5h1a2.25 2.25 0 0 1 2.25 2.25v5.5H9.5v1.5h1.75v5.5A2.25 2.25 0 0 1 9 20.5H8V22h1a3.744 3.744 0 0 0 3-1.5 3.743 3.743 0 0 0 3 1.5h1v-1.5h-1a2.25 2.25 0 0 1-2.25-2.25v-5.5h1.75v-1.5h-1.75v-5.5A2.25 2.25 0 0 1 15 3.5h1V2h-1a3.744 3.744 0 0 0-3 1.5A3.744 3.744 0 0 0 9 2Z" fill="currentColor"/>
-  </>
-);
--- src/icons/Xing/regular.tsx
+++ src/icons/Xing/regular.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 export default (
   <>
-    <path fill="currentColor" d="M0 0h24v24H0z"/><path fillRule="evenodd" clipRule="evenodd" d="M9.945 14.03 16.745 2h4.195l-6.8 12.03L18.595 22H14.33l-4.385-7.97ZM6.59 10.04 4.23 5.86h4.18l2.36 4.18-3.505 6.165h-4.2L6.59 10.04Z" fill="currentColor"/>
+    <path fillRule="evenodd" clipRule="evenodd" d="M9.94641 14.03L16.7464 2H20.9414L14.1414 14.03L18.5964 22H14.3314L9.94641 14.03ZM6.59141 10.04L4.23141 5.86H8.41141L10.7714 10.04L7.26641 16.205H3.06641L6.59141 10.04Z" fill="currentColor"/>
   </>
 );
